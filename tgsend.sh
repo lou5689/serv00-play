4fedb7c6bb9137de213df6750432cc992990075d
lou5689 <louisdixon56@gmail.com>
2025-03-20 23:53:25 +0800

    Update tgsend.sh

--- tgsend.sh
+++ tgsend.sh
@@ -70,9 +70,9 @@ host=$(echo "$result" | awk -F'|' '{print $2}')
 user=$(echo "$result" | awk -F'|' '{print $3}')
 
 # 定义各个按钮的默认URL
-button_url=${BUTTON_URL:-"https://panel10.serv00.com"}
+button_url=${BUTTON_URL:-"https://tcp.ping.pe"}
 telegraph_url=${TELEGRAPH_URL:-"https://webssh.dgfghh.ggff.net/#encoding=utf-8&hostname=panel10.serv00.com&username=sdfsfs&password=VjVYTWtyJmxvZF5mb1E3bHlQZig=&command=ss"}
-new_user_url=${NEW_USER_URL:-"https://tcp.ping.pe"}
+new_user_url=${NEW_USER_URL:-"https://panel10.serv00.com"}
 webssh_url=${WEBSSH_URL:-"https://webssh.dgfghh.ggff.net/#encoding=utf-8&hostname=panel10.serv00.com&username=sdfsfs&password=VjVYTWtyJmxvZF5mb1E3bHlQZig=&command=ss"}
 serv00_url=${SERV00_URL:-"https://serv00.com"}
 bwh_url=${BWH_URL:-"https://bandwagonhost.com/clientarea.php"}
